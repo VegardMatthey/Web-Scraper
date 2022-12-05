b2f065f0923320b3d97a79a79f5354b346ddd13b
Vegard Matthey <mattheyvegards@gmail.com>
2022-12-05 23:09:36 +0100

    updated paths

--- run.ps1
+++ run.ps1
@@ -1,10 +1,10 @@
-Set-Location C:\Users\matth\repo\web\
+cd ~\repo\web\
 python web.py
-rm C:\Users\matth\repo\web\parse\data\*
-mv C:\Users\matth\Downloads\html* C:\Users\matth\repo\web\parse\data\
-mv C:\Users\matth\Downloads\overview* C:\Users\matth\repo\web\parse\data\
-cd C:\Users\matth\repo\web\parse\
+rm ~\repo\web\parse\data\*
+mv ~\Downloads\html* ~\repo\web\parse\data\
+mv ~\Downloads\overview* ~\repo\web\parse\data\
+cd ~\repo\web\parse\
 cargo run -q
 cd ..
 python spread.py
-rm C:\Users\matth\repo\web\parse\data\*
+rm ~\repo\web\parse\data\*
